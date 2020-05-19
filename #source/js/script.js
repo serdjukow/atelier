e71364dcc2b37ebd9d40a83ff29e584d851a5dd0
Sergej <62400556+serdjukow@users.noreply.github.com>
2020-05-19 11:09:46 +0200

    Atelier 1.0

---
+++ #source/js/script.js
@@ -0,0 +1,15 @@
+	/*============== Slider ============================*/
+$(document).ready(function(){
+	$('.slider').slick({
+		dots:true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		adaptiveHeight:true,
+		speed:3000,
+		easing: 'leniar',
+		lazyLoad: 'ondemand',
+	});
+});
+	/*============== Slider ============================*/
