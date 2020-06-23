73164057baf2fb2e1e044d8b49428d17879421c9
Sergej <62400556+serdjukow@users.noreply.github.com>
2020-06-23 12:36:24 +0200

    U2

--- #source/js/script.js
+++ #source/js/script.js
@@ -1,15 +1,18 @@
 	/*============== Slider ============================*/
 $(document).ready(function(){
 	$('.slider').slick({
-		dots:true,
+		dots:false,
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		autoplay: true,
-		autoplaySpeed: 2000,
+		autoplaySpeed: 1500,
 		adaptiveHeight:true,
-		speed:3000,
+		speed:1500,
 		easing: 'leniar',
 		lazyLoad: 'ondemand',
+		fade: true,
+		infinite: true,
+		cssEase: 'linear'
 	});
 });
 	/*============== Slider ============================*/
---
+++ js/script.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".slider").slick({dots:!0,slidesToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:2e3,adaptiveHeight:!0,speed:3e3,easing:"leniar",lazyLoad:"ondemand"})});
